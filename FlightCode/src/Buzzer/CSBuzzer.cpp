0d9c5405b95580578084756caad7d535a730df86
sf314 <sf314@icloud.com>
2017-10-30 15:43:37 -0700

    Basic buzzer implementation
    Very rough, and could undergo paradigm shift later.

---
+++ FlightCode/src/Buzzer/CSBuzzer.cpp
@@ -0,0 +1,89 @@
+// CSBuzzer implementation file
+// Stephen Flores
+
+#include "CSBuzzer.h"
+
+// Initialization
+CSBuzzer::CSBuzzer() {
+    shouldDebug = false;
+    pin = 0;
+    isOn = false;
+    
+    songMode = false;
+    songStartTime = 0;
+    songCurrentTime = 0;
+}
+
+// Configuration
+void CSBuzzer::config(int p) {
+    pin = p;
+    pinMode(p, OUTPUT);
+    debugln("CSBuzzer: configured on pin " + String(p));
+}
+
+void CSBuzzer::setDebugMode(bool b) {
+    shouldDebug = b;
+}
+
+void CSBuzzer::debugln(String s) {
+    if (shouldDebug) {
+        Serial.println(s);
+    }
+}
+
+void CSBuzzer::debug(String s) {
+    if (shouldDebug) {
+        Serial.print(s);
+    }
+}
+
+void CSBuzzer::activate() {
+    isOn = true;
+    debugln("CSBuzzer: activated");
+}
+
+void CSBuzzer::deactivate() {
+    isOn = false;
+    debugln("CSBuzzer: deactivated");
+}
+
+void CSBuzzer::toggle() {
+    isOn = !isOn;
+    debugln("CSBuzzer: toggled to state " + String(isOn));
+}
+
+void CSBuzzer::playSong() {
+    songMode = !songMode;
+    debugln("CSBuzzer: songMode set to " + String(songMode));
+    if (songMode) {
+        // set start time for song
+        songStartTime = millis();
+    } else {
+        deactivate();
+    }
+}
+
+void CSBuzzer::main() {
+    // Guard songMode
+    if (songMode) {
+        // Run song process
+        sing();
+        return; // exit early
+    }
+    
+    if (isOn) {
+        digitalWrite(pin, HIGH);
+        tone(23, 700);
+    } else {
+        digitalWrite(pin, LOW);
+        tone(23, 700);
+    }
+    
+}
+
+void CSBuzzer::sing() {
+    songCurrentTime = millis() - songStartTime;
+    // Oh dear
+    // queue?
+    // Remove from queue when deltaT exceeds node time
+}
--- FlightCode/src/Buzzer/CSBuzzer.h
+++ FlightCode/src/Buzzer/CSBuzzer.h
@@ -1,2 +1,39 @@
 // CSBuzzer header file
-// Test
+// Stephen Flores
+
+#ifndef _CSBuzzer_h
+#define _CSBuzzer_h
+
+#include <Arduino.h>
+
+
+class CSBuzzer {
+public:
+    // initialization
+    CSBuzzer();
+    void config(int p);
+    void setDebugMode(bool b);
+    
+    // main
+    void main();
+    
+    // toggle mode
+    void activate(); // Takes effect in main();
+    void deactivate();
+    void toggle();
+    void playSong(); // time-dependent, run like main
+private:
+    bool shouldDebug;
+    bool isOn;
+    
+    int pin;
+    void debugln(String s);
+    void debug(String s);
+    
+    bool songMode;
+    long songStartTime;
+    long songCurrentTime;
+    void sing();
+};
+
+#endif
